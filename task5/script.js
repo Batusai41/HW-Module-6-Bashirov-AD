a387d7b06d0cfa096de71535e1c46dc18fb28439
Batusai <imbareaverdrop@mail.ru>
2022-04-26 01:08:54 +1200

    fixedfinal task5

--- task5/script.js
+++ task5/script.js
@@ -1,14 +1,13 @@
-let message = document.querySelector('#message');
-let duplicateField = document.querySelector('#duplicateField');
+let message = document.querySelector("#message");
+let duplicateField = document.querySelector("#duplicateField");
 message.addEventListener('input', function () {
     duplicateField.textContent = this.value;
 });
 
-
 let clear = document.querySelector("#clear");
-clear.onclick = function (click) {
+clear.addEventListener('click', function (click) {
     click.preventDefault();
     console.log(message.value);
     document.querySelector("#message").value = "";
-    document.querySelector("#duplicateField").value = "";
-};
+    duplicateField.textContent = this.value;
+});
